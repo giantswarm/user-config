2ecc4c0cfe6e873a2bec2736c73aebc54801880a
Tim Schindler <tim@catalyst-zero.com>
2014-12-05 10:08:51 +0000

    added tests

--- user_config_validator_test.go
+++ user_config_validator_test.go
@@ -133,32 +133,32 @@ var _ = Describe("user config validator", func() {
 		Describe("parsing app-config with unknown fields", func() {
 			BeforeEach(func() {
 				byteSlice = []byte(`{
-        "app_name": "test-app-name",
-        "services": [
-          {
-            "service_name": "session",
-            "components": [
-              {
-                "component_name": "api",
-                "image": "registry/namespace/repository:version",
-                "ports": [ "80/tcp" ],
-                "dependencies": [
-                  { "name": "redis", "port": 6379, "same_machine": true }
-                ],
-                "domains": { "test.domain.io": "80" }
-              },
-              {
-                "component_name": "redis",
-                "image": "dockerfile/redis",
-                "ports": [ "6379/tcp" ],
-                "volume": [
-                  { "path": "/data", "size": "5 GB" }
-                ]
-              }
-            ]
-          }
-        ]
-      }`)
+          "app_name": "test-app-name",
+          "services": [
+            {
+              "service_name": "session",
+              "components": [
+                {
+                  "component_name": "api",
+                  "image": "registry/namespace/repository:version",
+                  "ports": [ "80/tcp" ],
+                  "dependencies": [
+                    { "name": "redis", "port": 6379, "same_machine": true }
+                  ],
+                  "domains": { "test.domain.io": "80" }
+                },
+                {
+                  "component_name": "redis",
+                  "image": "dockerfile/redis",
+                  "ports": [ "6379/tcp" ],
+                  "volume": [
+                    { "path": "/data", "size": "5 GB" }
+                  ]
+                }
+              ]
+            }
+          ]
+        }`)
 
 				appConfig = userConfigPkg.AppConfig{}
 				err = json.Unmarshal(byteSlice, &appConfig)
@@ -175,79 +175,125 @@ var _ = Describe("user config validator", func() {
 		})
 
 		Describe("fix app-config fields", func() {
-			BeforeEach(func() {
-				byteSlice = []byte(`{
-          "appName": "test-app-name",
-          "Services": [
-            {
-              "service_name": "session",
-              "Components": [
-                {
-                  "component_name": "api",
-                  "image": "registry/namespace/repository:version",
-                  "ports": [ "80/tcp" ],
-                  "dependencies": [
-                    { "name": "redis", "port": 6379, "same_machine": true }
-                  ],
-                  "Domains": { "test.domain.io": "80" }
-                },
-                {
-                  "ComponentName": "redis",
-                  "image": "dockerfile/redis",
-                  "ports": [ "6379/tcp" ],
-                  "volumes": [
-                    { "path": "/data", "size": "5 GB" }
-                  ]
-                }
-              ]
-            }
-          ]
-        }`)
+			Describe("with actually valid field names", func() {
+				BeforeEach(func() {
+					byteSlice = []byte(`{
+            "appName": "test-app-name",
+            "Services": [
+              {
+                "service_name": "session",
+                "Components": [
+                  {
+                    "component_name": "api",
+                    "image": "registry/namespace/repository:version",
+                    "ports": [ "80/tcp" ],
+                    "dependencies": [
+                      { "name": "redis", "port": 6379, "same_machine": true }
+                    ],
+                    "Domains": { "test.domain.io": "80" }
+                  },
+                  {
+                    "ComponentName": "redis",
+                    "image": "dockerfile/redis",
+                    "ports": [ "6379/tcp" ],
+                    "volumes": [
+                      { "path": "/data", "size": "5 GB" }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }`)
 
-				appConfig = userConfigPkg.AppConfig{}
-				err = json.Unmarshal(byteSlice, &appConfig)
+					appConfig = userConfigPkg.AppConfig{}
+					err = json.Unmarshal(byteSlice, &appConfig)
+				})
+
+				It("should not throw error", func() {
+					Expect(err).To(BeNil())
+				})
+
+				It("should properly parse given app name", func() {
+					Expect(appConfig.AppName).To(Equal("test-app-name"))
+				})
+
+				It("should parse one service", func() {
+					Expect(appConfig.Services).To(HaveLen(1))
+				})
+
+				It("should parse one service", func() {
+					Expect(appConfig.Services[0].ServiceName).To(Equal("session"))
+				})
+
+				It("should parse two components", func() {
+					Expect(appConfig.Services[0].Components).To(HaveLen(2))
+				})
+
+				It("should parse one domain for component", func() {
+					Expect(appConfig.Services[0].Components[0].Domains).To(HaveLen(1))
+				})
+
+				It("should parse correct component domain", func() {
+					Expect(appConfig.Services[0].Components[0].Domains["test.domain.io"]).To(Equal("80"))
+				})
+
+				It("should parse correct component image 1", func() {
+					Expect(appConfig.Services[0].Components[0].Image.Registry).To(Equal("registry"))
+					Expect(appConfig.Services[0].Components[0].Image.Namespace).To(Equal("namespace"))
+					Expect(appConfig.Services[0].Components[0].Image.Repository).To(Equal("repository"))
+					Expect(appConfig.Services[0].Components[0].Image.Version).To(Equal("version"))
+				})
+
+				It("should parse correct component image 2", func() {
+					Expect(appConfig.Services[0].Components[1].Image.Registry).To(Equal(""))
+					Expect(appConfig.Services[0].Components[1].Image.Namespace).To(Equal("dockerfile"))
+					Expect(appConfig.Services[0].Components[1].Image.Repository).To(Equal("redis"))
+					Expect(appConfig.Services[0].Components[1].Image.Version).To(Equal(""))
+				})
 			})
 
-			It("should not throw error", func() {
-				Expect(err).To(BeNil())
-			})
+			Describe("with actually invalid field names", func() {
+				BeforeEach(func() {
+					byteSlice = []byte(`{
+            "appName": "test-app-name",
+            "Services": [
+              {
+                "service_name": "session",
+                "Components": [
+                  {
+                    "component_name": "api",
+                    "image": "registry/namespace/repository:version",
+                    "ports": [ "80/tcp" ],
+                    "dependencies": [
+                      { "name": "redis", "port": 6379, "same_machine": true }
+                    ],
+                    "Domains": { "test.domain.io": "80" }
+                  },
+                  {
+                    "ComponentNames": "redis",
+                    "image": "dockerfile/redis",
+                    "ports": [ "6379/tcp" ],
+                    "volumes": [
+                      { "path": "/data", "size": "5 GB" }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }`)
 
-			It("should properly parse given app name", func() {
-				Expect(appConfig.AppName).To(Equal("test-app-name"))
-			})
+					appConfig = userConfigPkg.AppConfig{}
+					err = json.Unmarshal(byteSlice, &appConfig)
+				})
 
-			It("should parse one service", func() {
-				Expect(appConfig.Services).To(HaveLen(1))
-			})
+				It("should throw error", func() {
+					Expect(userConfigPkg.IsErrUnknownJsonField(err)).To(BeTrue())
+					Expect(err.Error()).To(Equal(`Cannot parse app config. Unknown field '["services"][0]["components"][1]["component_names"]' detected.`))
+				})
 
-			It("should parse one service", func() {
-				Expect(appConfig.Services[0].ServiceName).To(Equal("session"))
-			})
-
-			It("should parse two components", func() {
-				Expect(appConfig.Services[0].Components).To(HaveLen(2))
-			})
-
-			It("should parse one domain for component", func() {
-				Expect(appConfig.Services[0].Components[0].Domains).To(HaveLen(1))
-			})
-
-			It("should parse correct component domain", func() {
-				Expect(appConfig.Services[0].Components[0].Domains["test.domain.io"]).To(Equal("80"))
-			})
-
-			It("should parse correct component image 1", func() {
-				Expect(appConfig.Services[0].Components[0].Image.Registry).To(Equal("registry"))
-				Expect(appConfig.Services[0].Components[0].Image.Namespace).To(Equal("namespace"))
-				Expect(appConfig.Services[0].Components[0].Image.Repository).To(Equal("repository"))
-				Expect(appConfig.Services[0].Components[0].Image.Version).To(Equal("version"))
-			})
-
-			It("should parse correct component image 2", func() {
-				Expect(appConfig.Services[0].Components[1].Image.Registry).To(Equal(""))
-				Expect(appConfig.Services[0].Components[1].Image.Namespace).To(Equal("dockerfile"))
-				Expect(appConfig.Services[0].Components[1].Image.Repository).To(Equal("redis"))
-				Expect(appConfig.Services[0].Components[1].Image.Version).To(Equal(""))
+				It("should not parse given app name", func() {
+					Expect(appConfig.AppName).To(Equal(""))
+				})
 			})
 		})
 	})
