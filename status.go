b10467b3fc365e479e7f9d715ff2ec4c4bc4e3fe
Stephan Zeissler <stephan@giantswarm.io>
2015-05-01 01:35:29 +0200

    Add STOPPING state

--- status.go
+++ status.go
@@ -7,25 +7,35 @@ const (
 	STATUS_DOWN     Status = "down"
 	STATUS_STARTING Status = "starting"
 	STATUS_UP       Status = "up"
+	STATUS_STOPPING Status = "stopping"
 )
 
 func (s *Status) String() string {
 	return string(*s)
 }
 
+var (
+	// StatusOrder is used by AggregateStatus to determine the higher status.
+	// Lower Index wins over higher Index
+	StatusOrder = []Status{
+		STATUS_FAILED,
+		STATUS_DOWN,
+		STATUS_STARTING,
+		STATUS_STOPPING,
+		STATUS_UP,
+	}
+)
+
 // AggregateStatus returns the 'higher' of the two status, given the following order:
-//  ok < starting < down < failed
+//  ok < stopping < starting < down < failed
 func AggregateStatus(status1, status2 Status) Status {
-	if status1 == STATUS_FAILED || status2 == STATUS_FAILED {
-		return STATUS_FAILED
+
+	for _, nextStatus := range StatusOrder {
+		if status1 == nextStatus || status2 == nextStatus {
+			return nextStatus
+		}
 	}
-	if status1 == STATUS_DOWN || status2 == STATUS_DOWN {
-		return STATUS_DOWN
-	}
-	if status1 == STATUS_STARTING || status2 == STATUS_STARTING {
-		return STATUS_STARTING
-	}
-	return STATUS_UP
+	panic("Unknown status: " + status1 + ", " + status2)
 }
 
 // Inactive means an app is failed or down.
@@ -33,14 +43,10 @@ func IsStatusInactive(status Status) bool {
 	return status == STATUS_FAILED || status == STATUS_DOWN
 }
 
-// Active means starting or up.
-func IsStatusActive(status Status) bool {
-	return status == STATUS_STARTING || status == STATUS_UP
-}
-
 // IsStatusFinal returns whether the given status is a final status and should not change upon itself.
-// E.g. A unit with a status STARTING will after some time either switch to UP or FAILED, thus the state is not final.
-// Final states are UP or FAILED.
+// E.g. A unit with a status STARTING will after some time either switch to UP or FAILED,
+// thus the state is not final.
+// Final states are UP, DOWN or FAILED.
 func IsStatusFinal(status Status) bool {
-	return status == STATUS_FAILED || status == STATUS_UP
+	return status == STATUS_FAILED || status == STATUS_UP || status == STATUS_DOWN
 }
