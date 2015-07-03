2ea32d0472d1b73b6e6ff9f6a7ede061b1ca22f3
Ewout Prangsma <ewout@prangsma.net>
2015-07-03 09:29:50 +0200

    Added Validate

--- pod.go
+++ pod.go
@@ -2,31 +2,38 @@ package userconfig
 
 import (
 	"encoding/json"
-
-	"github.com/juju/errgo"
 )
 
 // Type of the "pod" field in a V2 node definition.
 type PodEnum string
 
 const (
-	PodNone     = PodEnum("none")     // No pod is created and no resources are shared.
-	PodChildren = PodEnum("children") // A node defining this only configures its direct children to be placed into a pod.
-	PodInherit  = PodEnum("inherit")  // A node defining this configures all its children and grand-children to be placed into a pod.
+	PodNone     PodEnum = "none"     // No pod is created and no resources are shared.
+	PodChildren PodEnum = "children" // A node defining this only configures its direct children to be placed into a pod.
+	PodInherit  PodEnum = "inherit"  // A node defining this configures all its children and grand-children to be placed into a pod.
 )
 
 // UnmarshalJSON performs a validation during unmarshaling.
-func (this *PodEnum) UnmarshalJSON(data []byte) error {
+func (pe *PodEnum) UnmarshalJSON(data []byte) error {
 	var s string
 	err := json.Unmarshal(data, &s)
 	if err != nil {
 		return mask(err)
 	}
 
-	if s != string(PodNone) && s != string(PodChildren) && s != string(PodInherit) {
-		return mask(errgo.WithCausef(nil, InvalidPodConfigError, "Cannot parse app config. Invalid pod value '%s' detected.", s))
+	pv := PodEnum(s)
+	if err := pv.Validate(); err != nil {
+		return mask(err)
 	}
 
-	*this = PodEnum(s)
+	*pe = pv
+	return nil
+}
+
+// Validate checks that the given enum is a valid value.
+func (pe PodEnum) Validate() error {
+	if pe != PodNone && pe != PodChildren && pe != PodInherit {
+		return maskf(InvalidPodConfigError, "invalid pod value '%s'", string(pe))
+	}
 	return nil
 }
