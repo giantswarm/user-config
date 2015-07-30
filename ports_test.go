bdd71751529fe1c0610b0de311b8fcf430f9d948
Ewout Prangsma <ewout@prangsma.net>
2015-07-30 14:58:52 +0200

    Extended test with result content validation

--- ports_test.go
+++ ports_test.go
@@ -4,21 +4,33 @@ import (
 	"encoding/json"
 	"testing"
 
+	"github.com/giantswarm/generic-types-go"
 	"github.com/giantswarm/user-config"
 )
 
 func TestValidPortsValues(t *testing.T) {
-	list := []string{
-		`80`,
-		`"80/tcp"`,
-		`["80/tcp","81/tcp"]`,
-		`[80,"81/tcp","82"]`,
+	list := []struct {
+		Input  string
+		Result userconfig.PortDefinitions
+	}{
+		{`80`, userconfig.PortDefinitions{generictypes.MustParseDockerPort("80")}},
+		{`"80/tcp"`, userconfig.PortDefinitions{generictypes.MustParseDockerPort("80")}},
+		{`["80/tcp","81/tcp"]`, userconfig.PortDefinitions{generictypes.MustParseDockerPort("80"), generictypes.MustParseDockerPort("81")}},
+		{`[80,"81/tcp","82"]`, userconfig.PortDefinitions{generictypes.MustParseDockerPort("80"), generictypes.MustParseDockerPort("81"), generictypes.MustParseDockerPort("82")}},
 	}
 
-	for _, s := range list {
+	for _, test := range list {
 		var pds userconfig.PortDefinitions
-		if err := json.Unmarshal([]byte(s), &pds); err != nil {
-			t.Fatalf("Valid port definitions value '%s' considered invalid because %v", s, err)
+		if err := json.Unmarshal([]byte(test.Input), &pds); err != nil {
+			t.Fatalf("Valid port definitions value '%s' considered invalid because %v", test.Input, err)
+		}
+		if len(pds) != len(test.Result) {
+			t.Fatalf("Invalid length, expected %v, got %v", len(test.Result), len(pds))
+		}
+		for i, x := range pds {
+			if !x.Equals(test.Result[i]) {
+				t.Fatalf("Invalid element at %v, expected %v, got %v", i, test.Result[i], pds[i])
+			}
 		}
 	}
 }
